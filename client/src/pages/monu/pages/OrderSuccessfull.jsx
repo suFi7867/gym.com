f61d7245990a5d5c1ce9f045a31979610dc8f966
suFi7867 <noorani786.ss@gmail.com>
2022-12-16 21:08:15 +0530

    payment success

--- client/src/pages/monu/pages/OrderSuccessfull.jsx
+++ client/src/pages/monu/pages/OrderSuccessfull.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import { Button, VStack, Text, Image, HStack } from "@chakra-ui/react";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+//import pay from "../Styles/pay.png"
 
 const OrderSuccessfull = () => {
-  return (
-    <div>
-      <h1>OrderSuccessfull</h1>
-    </div>
-  )
-}
 
-export default OrderSuccessfull
+    const navigate = useNavigate();
+
+    return (
+        <div>
+            <VStack margin="auto"
+             spacing={10} position={"relative"} overflow="hidden"
+                bgGradient="radial-gradient(circle, rgba(0,0,0,1) 7%, rgba(64,64,64,1) 100%)" style={{ width: "100%", height: "100vh" }} >
+                <div>
+                    <Text style={{ color: "orange", fontSize: "2.5rem", fontWeight: "bold" }}>Hurray !! Order Successful</Text>
+                </div>
+                <div>
+                    <img
+                        src="https://www.pinkwigscloset.com/images/success-tick.gif"
+                        width="500px"
+                        alt="img"
+                    />
+                </div>
+
+              {/*  <Image w="80%" position={"absolute"} top="0" src={pay} /> */}
+                <Button width="30%" height="50px" onClick={() => navigate("/")} fontSize="x" padding={3} w="30%" colorScheme='orange'> CONTINUE SHOPPING </Button>
+            </VStack>
+        </div>
+    );
+};
+
+export default OrderSuccessfull;
